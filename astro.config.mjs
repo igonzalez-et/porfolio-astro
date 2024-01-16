d2498d9059505340cd5d9082cd4013783ab7de46
igonzalez-et <igonzalezvirsa@gmail.com>
2024-01-16 13:12:55 +0100

    Multilenguaje, años y proyectos en grande como badges

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://igonzalez-et.github.io',
   base: '/porfolio-astro',
-})
+  integrations: [tailwind()]
+});
